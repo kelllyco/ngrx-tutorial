c81f3d4528b51fbfa3cb4279a45c770f5c5f98ef
kellycochran <kac1011@icloud.com>
2024-06-11 13:51:59 -0400

    lesson 20

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,15 +1,29 @@
 import { Injectable } from "@angular/core";
-import { Actions } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { AuthActions } from "./action-types";
+import { tap } from "rxjs/operators";
 
 @Injectable() // class should not be injected anywhere in code, 
 // ngrx is the only one to use it (except in auth.module)
 export class AuthEffects {
+    
+    // takes function as input argument, returns side effect observable
+    login$ = createEffect(() => 
+            this.actions$
+                .pipe(
+                    ofType(AuthActions.login),
+                    tap(action => localStorage.setItem('user', 
+                        JSON.stringify(action.user))
+                    )
+                ),
+                {dispatch: false}); // the false is important, says no action is dispatched, IMPT TO ADD
+
     constructor(private actions$: Actions) { // Actions is an observable from ngrx effects
-        actions$.subscribe(action => {
-            if (action.type == '[Login Page] User Login') {
-                localStorage.setItem('user', 
-                JSON.stringify(action["user"]));
-            }
-        });
+
+        
+
+        
+
+
     }
 }
