c820465756a7ae0409afe8253cf62f08aef8faf3
kellycochran <kac1011@icloud.com>
2024-06-11 17:04:05 -0400

    lessons 29 and 30

---
+++ src/app/courses/course/reducers/course.reducers.ts
@@ -0,0 +1,25 @@
+import { EntityState, createEntityAdapter } from "@ngrx/entity";
+import { Course } from "../../model/course";
+import { createReducer, on } from "@ngrx/store";
+import { CourseActions } from "../../action-types";
+
+export interface CoursesState extends EntityState<Course>{
+    // entity format!
+    // entities: {[key:number]:Course} // courses should not be stored in state as an array, transform here into key val pair
+    // ids: number[] // defines entity order
+
+    // entity state does above much easier, gives a dict and ids which corresponds to natural order
+}
+
+// adapter provides traditional CRUD ops to the entity!!
+export const adapter = createEntityAdapter<Course>();
+
+// empty entities map and empty ids array
+export const initialCoursesState = adapter.getInitialState(); 
+
+export const coursesReducer = createReducer(
+    initialCoursesState,
+
+    on(CourseActions.allCoursesLoaded, 
+        (state, action) => adapter.addMany(action.courses, state))
+);
