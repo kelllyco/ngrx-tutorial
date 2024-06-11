d10eddc104ea8c19b1414a68f6ffb0839125211a
kellycochran <kac1011@icloud.com>
2024-06-11 15:07:37 -0400

    lesson 21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Route
 import { AuthState } from './auth/reducers';
 import { AppState } from './reducers';
 import { isLoggedIn, isLoggedOut } from './auth/auth.selectors';
-import { logout } from './auth/auth.actions';
+import { login, logout } from './auth/auth.actions';
 
 @Component({
   selector: 'app-root',
@@ -26,6 +26,12 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit() {
+
+      const userProfile = localStorage.getItem("user");
+
+      if (userProfile) {
+        this.store.dispatch(login({user: JSON.parse(userProfile)}));
+      }
 
       this.router.events.subscribe(event  => {
         switch (true) {
--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { AuthActions } from "./action-types";
 import { tap } from "rxjs/operators";
+import { Router } from "@angular/router";
 
 @Injectable() // class should not be injected anywhere in code, 
 // ngrx is the only one to use it (except in auth.module)
@@ -18,7 +19,21 @@ export class AuthEffects {
                 ),
                 {dispatch: false}); // the false is important, says no action is dispatched, IMPT TO ADD
 
-    constructor(private actions$: Actions) { // Actions is an observable from ngrx effects
+
+    logout$ = createEffect(() => 
+            this.actions$
+                .pipe(
+                    ofType(AuthActions.logout),
+                    tap(action => {
+                        localStorage.removeItem('user');
+                        this.route.navigateByUrl('/login');
+
+                    })
+                ),
+                {dispatch: false});
+
+    constructor(private actions$: Actions,
+            private route: Router) { // Actions is an observable from ngrx effects
 
         
 
