45b64c590d2c3602d0d716e9587e1eead0767886
kellycochran <kac1011@icloud.com>
2024-06-14 11:51:09 -0400

    lessons 44 and 45

--- src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
@@ -69,6 +69,16 @@ export class EditCourseDialogComponent {
 
       this.dialogRef.close();
     }
+    else if (this.mode == 'create') {
+      this.coursesService.add(course)
+        .subscribe(
+          newCourse => {
+            console.log("new course", newCourse);
+
+            this.dialogRef.close();
+          }
+        );
+    }
   }
 
 
